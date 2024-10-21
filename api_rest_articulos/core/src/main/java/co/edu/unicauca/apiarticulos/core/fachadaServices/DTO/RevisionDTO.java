fe537fce7d72bb23aa95e153024457314fae857d
Santiago <santidorado@unicauca.edu.co>
2024-10-21 17:21:22 -0500

    Modificaciones

--- api_rest_articulos/core/src/main/java/co/edu/unicauca/apiarticulos/core/fachadaServices/DTO/RevisionDTO.java
+++ api_rest_articulos/core/src/main/java/co/edu/unicauca/apiarticulos/core/fachadaServices/DTO/RevisionDTO.java
@@ -9,12 +9,12 @@ import lombok.Setter;
 @Getter @Setter @AllArgsConstructor
 public class RevisionDTO {
     private Integer id;
-    private ArticuloDTO Articulo;
+    private ArticuloRevisionDTO Articulo;
     private List<Integer> evaluadores;
     private List<String> comentarios;
     private String estado;
 
-    public RevisionDTO(){
-
+    public RevisionDTO() {
+ 
     } 
 }
